692ebb2d1a144a7e1853b00e368c0d6c1c130bc6
jm-19jimp <108098039+jm-19jimp@users.noreply.github.com>
2022-06-24 17:40:47 +0800

    Add files via upload

---
+++ External.js
@@ -0,0 +1,5 @@
+// A function to display a message function sayImJava()
+{
+alert("I\'m JavaScript!");
+} // Call function on click of the button
+document.getElementByld("myBtn").onclick - sayImJava;
